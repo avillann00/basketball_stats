5676967bbcd1f43f43324c1480034fb712f7edf8
AustinVillanueva <avillann00@gmail.com>
2025-02-13 22:02:33 -0500

    Tenth Commit
    In this commit I set up an early version of the App.js file so that it
    is currently handling the Login and Register pages that I also
    completed. In the future I will have to probably change the Login and
    Register files to handle OAuth but currently everything should be
    working.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import ProtectedRoute from './components/ProtectedRoute'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './components/AuthContext'
+import NotFound from "./pages/NotFound"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider> 
+      <BrowserRouter>
+        <Routes>
+          <Route path='*' element={<NotFound />} />
+          <Route
+            path='/login'
+            element={
+              <ProtectedRoute>
+                <Login />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/register'
+            element={
+              <ProtectedRoute>
+                <Register />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,43 @@
+import React, { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import AuthContext from '../components/AuthContext';
+
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+  const { loginUser } = useContext(AuthContext);
+
+  function goToRegister() {
+    navigate('register/')
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    await loginUser(username, password)
+    navigate('/')
+  }
+
+  return (
+    <div>
+      <h1>Hi, Welcome To The Login Page</h1>
+      <h1>Please Enter Your Username And Password</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          placeholder='username'
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type='text'
+          placeholder='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type='Submit'>Login</button>
+      </form>
+      <button onClick={goToRegister}>Need An Account?</button>
+    </div>
+  )
+}
